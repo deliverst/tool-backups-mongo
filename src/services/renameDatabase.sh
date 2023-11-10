158b734cd0dca690a323e670494bf48053688110
Javier Valles <deliverst@icloud.com>
2023-11-09 19:27:53 -0600

    add new service that allow rename database status is beta

---
+++ src/services/renameDatabase.sh
@@ -0,0 +1,16 @@
+# details: This code "rename" the database in mongodb, export data, and then import data again, but the new name"
+# date: 28/09/2023
+# by: deliverst
+# status: BETA
+# todo:
+
+# 1.- delete database in mongoose
+# 2.- clean the folders created when dumped
+
+nameCurrentDatabas="humberto-app"
+newNameDatabase="grupo-nevarez"
+folderToSaveDatabase="temp"
+
+mongodump -d "$nameCurrentDatabas" -o "$folderToSaveDatabase"
+mv "$folderToSaveDatabase/$nameCurrentDatabas" "$folderToSaveDatabase/$newNameDatabase"
+mongorestore --nsInclude="$newNameDatabase.*" "$folderToSaveDatabase"
